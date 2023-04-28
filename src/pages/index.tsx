4b2358e37203562c04f14a5dce3de1d04f5d3ac8
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2023-04-28 16:09:58 -0300

    style homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,36 @@
 import Head from 'next/head'
 
+import styles from '@/styles/styles.module.scss'
+
 export default function Home() {
   return (
     <>
       <Head>
         <title>Organize your life</title>
-        <meta name="description" content="Board app to organize your todos" />
+        <meta name="description" content="A tool for your everyday life Write, plan and organize yourself" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <main className={styles.contentContainer}>
+
+        <img src="/images/board-user.svg" alt="hero" />
+
+        <section className={styles.callToAction}>
+          <h1>A tool for your everyday life. Write, plan and organize yourself...</h1>
+          <p>
+            <span>100% free</span>
+          </p>
+        </section>
+
+        <div className={styles.donaters}>
+          <img src='https://placehold.co/150x150' alt="User photo" />
+          <img src='https://placehold.co/150x150' alt="User photo" />
+          <img src='https://placehold.co/150x150' alt="User photo" />
+          <img src='https://placehold.co/150x150' alt="User photo" />
+          <img src='https://placehold.co/150x150' alt="User photo" />
+        </div>
+
+      </main>
     </>
   )
 }
