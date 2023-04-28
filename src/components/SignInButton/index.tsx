f0440b729f69e3e127a1d3bb17c370a3233a89da
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2023-04-28 15:57:15 -0300

    add styling

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,7 +1,29 @@
+import styles from './styles.module.scss'
+import { FaGithub } from 'react-icons/fa'
+import { FiX } from 'react-icons/fi'
+
 export default function SignInButton() {
-  return (
-    <button>
-      Sign In With Github
-    </button>
+
+  const isUserLoggedIn = true;
+
+  return isUserLoggedIn ? (
+    <button
+    type="button"
+    className={styles.signInButton}
+    onClick={() => { }}
+  >
+    <img src='https://placehold.co/150x150' alt="User photo" />
+    Hi, User
+    <FiX color="#737380" className={styles.closeIcon} />
+  </button>
+  ) : (
+    <button
+    type="button"
+    className={styles.signInButton}
+    onClick={() => { }}
+  >
+    <FaGithub color="#ffb800" />
+    Sign In With Github
+  </button>
   )
 }
