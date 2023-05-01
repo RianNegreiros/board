00d01f92d008e84e2b05e89d8f729629862e3cbf
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2023-05-01 14:55:53 -0300

    config firebase configuration

---
+++ src/services/firebaseConnection.ts
@@ -0,0 +1,16 @@
+import { initializeApp, getApps } from "firebase/app"
+import 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCnkSwZeV66FxM7brIQWoJoRFOlIHAuM_8",
+  authDomain: "boardapp-47607.firebaseapp.com",
+  projectId: "boardapp-47607",
+  storageBucket: "boardapp-47607.appspot.com",
+  messagingSenderId: "980316387658",
+  appId: "1:980316387658:web:a0c8da3d607894d4ece9b3",
+  measurementId: "G-KDN2S4E1K5"
+}
+
+const firebase = getApps().length ? getApps()[0] : initializeApp(firebaseConfig)
+
+export default firebase
