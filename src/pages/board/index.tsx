080ea5b59238ad9086d867159263118f7b02ce01
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2023-05-01 16:41:57 -0300

    list tasks

--- src/pages/board/index.tsx
+++ src/pages/board/index.tsx
@@ -2,43 +2,67 @@ import { useState, FormEvent } from 'react'
 import Head from 'next/head'
 import { GetServerSideProps } from 'next'
 import { getSession } from 'next-auth/client'
+import Link from 'next/link'
 
 import styles from './styles.module.scss'
 import { FiPlus, FiCalendar, FiEdit2, FiTrash, FiClock } from 'react-icons/fi'
 import { SupportButton } from '@/components/SupportButton'
+import { format } from 'date-fns'
 
 import firebase from '@/services/firebaseConnection'
+
+type Task = {
+  id: string
+  created: Date
+  createdFormated: string
+  task: string
+  userId: string
+  name: string
+}
 
 interface BoardProps {
   user: {
     id: string
-    nome: string
+    name: string
   }
 }
 
 export default function Board({ user }: BoardProps) {
   const [input, setInput] = useState('')
+  const [taskList, setTaskList] = useState<Task[]>([])
 
   async function handleAddTask(e: FormEvent) {
     e.preventDefault()
 
     if (input === '') {
-      alert('Type a task')
+      alert('Type something!')
       return
     }
 
     await firebase.firestore().collection('tasks')
       .add({
         created: new Date(),
-        tarefa: input,
+        task: input,
         userId: user.id,
-        nome: user.nome
+        name: user.name
       })
       .then((doc) => {
-        console.log('Success: ', doc)
+        console.log('SUCCESS: ', doc)
+        let data = {
+          id: doc.id,
+          created: new Date(),
+          createdFormated: format(new Date(), 'dd MMMM yyyy'),
+          task: input,
+          userId: user.id,
+          name: user.name
+        }
+
+        setTaskList([...taskList, data])
+        setInput('')
+
       })
       .catch((err) => {
-        console.log('Error: ', err)
+        console.log('ERROR: ', err)
       })
   }
 
@@ -51,7 +75,7 @@ export default function Board({ user }: BoardProps) {
         <form onSubmit={handleAddTask} >
           <input
             type="text"
-            placeholder="Type a task..."
+            placeholder="Type your task here..."
             value={input}
             onChange={(e) => setInput(e.target.value)}
           />
@@ -60,35 +84,39 @@ export default function Board({ user }: BoardProps) {
           </button>
         </form>
 
-        <h1>You have 2 tasks</h1>
+        <h1>You have 2 tasks!</h1>
 
         <section>
-          <article className={styles.taskList}>
-            <p>Learn</p>
-            <div className={styles.actions}>
-              <div>
+          {taskList.map(task => (
+            <article className={styles.taskList}>
+              <Link href={`/board/${task.id}`}>
+                <p>{task.task}</p>
+              </Link>
+              <div className={styles.actions}>
                 <div>
-                  <FiCalendar size={20} color="#FFB800" />
-                  <time>July 17 2021</time>
+                  <div>
+                    <FiCalendar size={20} color="#FFB800" />
+                    <time>{task.createdFormated}</time>
+                  </div>
+                  <button>
+                    <FiEdit2 size={20} color="#FFF" />
+                    <span>Edit</span>
+                  </button>
                 </div>
+
                 <button>
-                  <FiEdit2 size={20} color="#FFF" />
-                  <span>Edit</span>
+                  <FiTrash size={20} color="#FF3636" />
+                  <span>Delete</span>
                 </button>
               </div>
-
-              <button>
-                <FiTrash size={20} color="#FF3636" />
-                <span>Delete</span>
-              </button>
-            </div>
-          </article>
+            </article>
+          ))}
         </section>
 
       </main>
 
       <div className={styles.vipContainer}>
-        <h3>Thank you for supporting</h3>
+        <h3>Thank you for supporting this project</h3>
         <div>
           <FiClock size={28} color="#FFF" />
           <time>
@@ -103,7 +131,6 @@ export default function Board({ user }: BoardProps) {
   )
 }
 
-
 export const getServerSideProps: GetServerSideProps = async ({ req }) => {
   const session = await getSession({ req })
 
@@ -117,7 +144,7 @@ export const getServerSideProps: GetServerSideProps = async ({ req }) => {
   }
 
   const user = {
-    nome: session?.user!.name,
+    name: session?.user!.name,
     id: session?.id
   }
 
