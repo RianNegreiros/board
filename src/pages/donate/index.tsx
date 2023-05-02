103c792fe2f9ce3e91c28091127aa16cebe4b273
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2023-05-02 16:44:19 -0300

    handle donations on donate page

--- src/pages/donate/index.tsx
+++ src/pages/donate/index.tsx
@@ -1,46 +1,87 @@
 
-import styles from './styles.module.scss'
-import Head from 'next/head'
 import { GetServerSideProps } from 'next'
+import { useState } from 'react' 
+import styles from './styles.module.scss'
+import Head from 'next/head' 
 import { getSession } from 'next-auth/client'
+import firebase from '@/services/firebaseConnection'
 
+import { PayPalButtons } from '@paypal/react-paypal-js'
 
-interface DonatePorps {
-  user: {
+interface DonatePorps{
+  user:{
     nome: string
     id: string
-    image: string
+    image: string 
   }
 }
 
-export default function Donate({ user }: DonatePorps) {
-  return (
-    <>
-      <Head>
-        <title>Help this project with $1,00 donation</title>
-      </Head>
-      <main className={styles.container}>
-        <img src="/images/rocket.svg" alt="Support this project" />
+export default function Donate({ user }: DonatePorps ){
+  const [vip, setVip] = useState(false)
 
+  async function handleSaveDonate(){
+    await firebase.firestore().collection('users')
+    .doc(user.id)
+    .set({
+      donate: true,
+      lastDonate: new Date(),
+      image: user.image
+    })
+    .then(()=>{
+      setVip(true)
+    })
+  }
+
+  return(
+    <>
+    <Head>
+      <title>Help this project</title>
+    </Head>
+    <main className={styles.container}>
+      <img src="/images/rocket.svg" alt="Support" />
+      
+      {vip && (
         <div className={styles.vip}>
           <img src={user.image} alt="User profile image" />
           <span>Thank you for supporting this project</span>
         </div>
+      )}
 
-        <h1>Support this project 🏆</h1>
-        <h3>Support with only: <span>$ 1,00</span></h3>
-        <strong>Get exclusive fuctionalities</strong>
-      </main>
+      <h1>Support this project 🏆</h1>
+      <h3>Support with only <span>R$ 1,00</span></h3>
+      <strong>Get exclusive fuctionalities</strong>
+
+      <PayPalButtons
+        createOrder={ (data, actions) => {
+          return actions.order.create({
+            purchase_units: [{
+              amount:{
+                value: '1'
+              }
+            }]
+          })
+        }}
+        onApprove={ (data, actions) => {
+          return actions.order!.capture().then(function(details){
+            console.log('Success: ' + details.payer.name!.given_name)
+            handleSaveDonate()
+          })
+        }}
+
+      />
+      
+    </main>    
     </>
   )
 }
 
+
 export const getServerSideProps: GetServerSideProps = async ({ req }) => {
   const session = await getSession({ req })
 
-  if (!session?.id) {
-    return {
-      redirect: {
+  if(!session?.id){
+    return{
+      redirect:{
         destination: '/',
         permanent: false
       }
@@ -53,8 +94,8 @@ export const getServerSideProps: GetServerSideProps = async ({ req }) => {
     image: session?.user?.image
   }
 
-  return {
-    props: {
+  return{
+    props:{
       user
     }
   }
